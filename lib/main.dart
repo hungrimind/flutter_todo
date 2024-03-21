1f8baa1a8fd90f6786b81e43eaf1956659575bcc
Tadas Petra <tadas@tadaspetra.com>
2024-03-21 08:17:00 -0500

    Looking sweet

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo/provider.dart';
 import 'package:todo/todo.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: const MyHomePage(),
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      colorScheme: const ColorScheme.light(
-        primary: Colors.black,
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(Provider(
+    notifier: TodoListNotifier([]),
+    child: MaterialApp(
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Colors.black,
+        ),
       ),
     ),
   ));
@@ -24,33 +26,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late SharedPreferences prefs;
-  List<Todo> _todoList = [];
   bool showDone = false;
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _retrieveTodos();
-  }
-
-  _retrieveTodos() async {
-    prefs = await SharedPreferences.getInstance();
-    final String? storedTodoList = prefs.getString('todoList');
-    if (storedTodoList != null) {
-      setState(() {
-        _todoList = (jsonDecode(storedTodoList) as List)
-            .map((todo) => Todo.fromJson(todo))
-            .toList();
-      });
-    }
-  }
-
-  _saveTodos() async {
-    await prefs.setString('todoList', jsonEncode(_todoList));
-  }
 
   @override
   void dispose() {
@@ -85,16 +63,15 @@ class _MyHomePageState extends State<MyHomePage> {
           actions: [
             TextButton(
               onPressed: () {
-                setState(() {
-                  _todoList.add(Todo(
+                Provider.of<TodoListNotifier>(context).add(
+                  Todo(
                     title: _titleController.text,
                     description: _descriptionController.text,
-                  ));
-                });
+                  ),
+                );
 
                 _titleController.clear();
                 _descriptionController.clear();
-                _saveTodos();
                 Navigator.pop(context);
               },
               child: const Text("Add"),
@@ -105,31 +82,16 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _deleteTodo(int index) {
-    setState(() {
-      _todoList.removeAt(index);
-    });
-    _saveTodos();
-  }
-
-  void _updateTodo(int index, bool? value) {
-    setState(() {
-      _todoList[index] = Todo(
-        title: _todoList[index].title,
-        description: _todoList[index].description,
-        isDone: value!,
-      );
-    });
-    _saveTodos();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final todoListNotifier = Provider.of<TodoListNotifier>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Todo"),
         actions: [
-          if (_todoList.where((element) => element.isDone).isNotEmpty)
+          if (todoListNotifier.value
+              .where((element) => element.isDone)
+              .isNotEmpty)
             TextButton(
               onPressed: () {
                 setState(() {
@@ -143,21 +105,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: ListView.builder(
-          itemCount: _todoList.length,
+          itemCount: todoListNotifier.value.length,
           itemBuilder: (context, index) {
-            if (_todoList[index].isDone && !showDone) return Container();
+            if (todoListNotifier.value[index].isDone && !showDone) {
+              return Container();
+            }
             return ListTile(
-              title: Text(_todoList[index].title),
-              subtitle: _todoList[index].description != null
-                  ? Text(_todoList[index].description!)
+              title: Text(todoListNotifier.value[index].title),
+              subtitle: todoListNotifier.value[index].description != null
+                  ? Text(todoListNotifier.value[index].description!)
                   : null,
               trailing: Checkbox(
-                value: _todoList[index].isDone,
-                onChanged: (bool? value) {
-                  _updateTodo(index, value);
-                },
+                value: todoListNotifier.value[index].isDone,
+                onChanged: (bool? value) =>
+                    todoListNotifier.toggleDone(todoListNotifier.value[index]),
               ),
-              onLongPress: () => _deleteTodo(index),
+              onLongPress: () =>
+                  todoListNotifier.remove(todoListNotifier.value[index]),
             );
           },
         ),
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
 
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Todo {
   final String title;
   final String? description;
@@ -30,4 +33,55 @@ class Todo {
   String toJson() => json.encode(toMap());
 
   factory Todo.fromJson(String source) => Todo.fromMap(json.decode(source));
+
+  Todo copyWith({
+    String? title,
+    String? description,
+    bool? isDone,
+  }) {
+    return Todo(
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+}
+
+class TodoListNotifier extends ValueNotifier<List<Todo>> {
+  TodoListNotifier(super.state) {
+    _initializeTodos();
+  }
+
+  late SharedPreferences prefs;
+
+  void _initializeTodos() async {
+    prefs = await SharedPreferences.getInstance();
+    final String? storedTodoList = prefs.getString('todoList');
+    if (storedTodoList != null) {
+      value = (jsonDecode(storedTodoList) as List)
+          .map((todo) => Todo.fromJson(todo))
+          .toList();
+    }
+  }
+
+  void add(Todo todo) async {
+    value = [...value, todo];
+    await prefs.setString('todoList', jsonEncode(value));
+  }
+
+  void remove(Todo todo) async {
+    value = value.where((element) => element != todo).toList();
+    await prefs.setString('todoList', jsonEncode(value));
+  }
+
+  void toggleDone(Todo todo) async {
+    value = value.map((oldTodo) {
+      if (oldTodo == todo) {
+        return oldTodo.copyWith(isDone: !oldTodo.isDone);
+      }
+      return oldTodo;
+    }).toList();
+
+    await prefs.setString('todoList', jsonEncode(value));
+  }
 }
