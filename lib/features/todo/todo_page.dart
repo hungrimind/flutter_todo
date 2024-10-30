0db146c3e9f19ca0e07de4889671be9540459072
Tadas Petra <tadas@tadaspetra.com>
2024-10-30 07:48:17 -0500

    remove unnecessary widgets

--- lib/features/todo/todo_page.dart
+++ lib/features/todo/todo_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todo/features/todo/todo.dart';
 import 'package:todo/features/todo/todo_page_view_model.dart';
 import 'package:todo/shared/date_service.dart';
 import 'package:todo/shared/locator.dart';
-import 'package:todo/shared/ui_utilities/value_listenable_builder_x.dart';
 
 class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
@@ -100,11 +98,34 @@ class _TodoPageState extends State<TodoPage> {
           ),
         ],
       ),
-      body: TodoList(
-        toggleDone: homePageViewModel.toggleDone,
-        removeTodo: homePageViewModel.remove,
-        todosNotifier: homePageViewModel.todosNotifier,
-        showCompletedTodos: homePageViewModel.showCompletedTodosNotifier,
+      body: ValueListenableBuilder(
+        valueListenable: homePageViewModel.todosNotifier,
+        builder: (context, todos, child) {
+          return ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+              return ValueListenableBuilder(
+                valueListenable: homePageViewModel.showCompletedTodosNotifier,
+                builder: (context, showCompletedTodos, child) {
+                  if (todo.completed && !showCompletedTodos) {
+                    return const SizedBox();
+                  }
+
+                  return ListTile(
+                    title: Text(todo.title),
+                    trailing: Checkbox(
+                      value: todo.completed,
+                      onChanged: (bool? value) =>
+                          homePageViewModel.toggleDone(todo),
+                    ),
+                    onLongPress: () => homePageViewModel.remove(todo),
+                  );
+                },
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -120,78 +141,6 @@ class _TodoPageState extends State<TodoPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class TodoList extends StatelessWidget {
-  const TodoList({
-    super.key,
-    required this.todosNotifier,
-    required this.showCompletedTodos,
-    required this.toggleDone,
-    required this.removeTodo,
-  });
-
-  final ValueNotifier<List<Todo>> todosNotifier;
-  final ValueNotifier<bool> showCompletedTodos;
-  final void Function(Todo todo) toggleDone;
-  final void Function(Todo todo) removeTodo;
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: todosNotifier,
-      builder: (context, todos, child) {
-        return ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (context, index) {
-            final todo = todos[index];
-            return TodoItem(
-              showCompletedTodos: showCompletedTodos,
-              todo: todo,
-              toggleDone: toggleDone,
-              removeTodo: removeTodo,
-            );
-          },
-        );
-      },
-    );
-  }
-}
-
-class TodoItem extends StatelessWidget {
-  const TodoItem({
-    super.key,
-    required this.showCompletedTodos,
-    required this.todo,
-    required this.toggleDone,
-    required this.removeTodo,
-  });
-
-  final ValueNotifier<bool> showCompletedTodos;
-  final Todo todo;
-  final void Function(Todo todo) toggleDone;
-  final void Function(Todo todo) removeTodo;
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: showCompletedTodos,
-      builder: (context, showCompletedTodos, child) {
-        if (todo.completed && !showCompletedTodos) {
-          return const SizedBox();
-        }
-
-        return ListTile(
-          title: Text(todo.title),
-          trailing: Checkbox(
-            value: todo.completed,
-            onChanged: (bool? value) => toggleDone(todo),
-          ),
-          onLongPress: () => removeTodo(todo),
-        );
-      },
     );
   }
 }
