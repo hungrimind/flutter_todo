161f76d51709e92be47dc3a6ededa079507053eb
Tadas Petra <tadas@tadaspetra.com>
2024-11-26 08:24:59 -0600

    Added golden test

---
+++ test/golden_test.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todo/main.dart';
+import 'package:flutter_todo/todo/todo_page.dart';
+import 'package:flutter_todo/utils/locator.dart';
+
+void main() {
+  group(MyApp, () {
+    setUp(() {
+      setupLocator();
+    });
+    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+
+      await expectLater(
+          find.byType(MyApp), matchesGoldenFile('goldens/no_todos.png'));
+
+      // Tap the add button
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pumpAndSettle();
+
+      // Enter text in the dialog
+      await tester.enterText(find.byType(TextField), 'Test Todo');
+      await tester.pumpAndSettle();
+
+      // Tap the Add button in the dialog
+      await tester.tap(find.byKey(TodoPage.popupAddTodoKey));
+      await tester.pumpAndSettle();
+
+      // Find the checkbox and verify initial state
+      final checkbox = find.byType(Checkbox);
+      expect(checkbox, findsOneWidget);
+      expect(tester.widget<Checkbox>(checkbox).value, false);
+
+      // Tap the checkbox
+      await tester.tap(checkbox);
+      await tester.pumpAndSettle();
+
+      // Tap the add button
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pumpAndSettle();
+
+      // Enter text in the dialog
+      await tester.enterText(find.byType(TextField), 'Test Todo 2');
+      await tester.pumpAndSettle();
+
+      // Tap the Add button in the dialog
+      await tester.tap(find.byKey(TodoPage.popupAddTodoKey));
+      await tester.pumpAndSettle();
+
+      // Toggle the show completed todos
+      await tester.tap(find.text('Show Done'));
+      await tester.pumpAndSettle();
+
+      await expectLater(
+          find.byType(MyApp), matchesGoldenFile('goldens/two_todos.png'));
+    });
+  });
+}
