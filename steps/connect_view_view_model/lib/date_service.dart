7dcd99cbaabe576b5a8a2ef5c6b5c4a4565a5e70
Robert Brunhage <robertbrunhage@gmail.com>
2025-01-05 17:01:25 +0100

    chore: lesson about service

--- steps/connect_view_view_model/lib/date_service.dart
+++ steps/connect_view_view_model/lib/date_service.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 class DateService {
   DateService();
+
+  final ValueNotifier<DateTime> dateNotifier = ValueNotifier(DateTime(2024, 12, 1));
+
+  void resetDate() {
+  }
 }
--- steps/connect_view_view_model/lib/utils/locator.dart
+++
@@ -1,2 +0,0 @@
-import 'package:demo/date_service.dart';
-import 'package:get_it/get_it.dart';
