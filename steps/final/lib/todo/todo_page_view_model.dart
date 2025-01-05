a5d10de387868dfbdd920abbd81ab79284ed5cab
Robert Brunhage <robertbrunhage@gmail.com>
2025-01-05 15:05:56 +0100

    chore: update folder naming

---
+++ steps/final/lib/todo/todo_page_view_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:demo/date_service.dart';
+import 'package:uuid/uuid.dart';
+
+import 'todo.dart';
+
+class TodoPageViewModel {
+  TodoPageViewModel({required DateService dateService})
+      : _dateService = dateService;
+
+  final DateService _dateService;
+
+  ValueNotifier<DateTime> get dateNotifier => _dateService.dateNotifier;
+
+  final ValueNotifier<List<Todo>> todosNotifier = ValueNotifier([]);
+  final ValueNotifier<bool> showCompletedTodosNotifier = ValueNotifier(false);
+
+  void add(String title) {
+    todosNotifier.value = [
+      ...todosNotifier.value,
+      Todo(id: const Uuid().v4(), title: title, completed: false),
+    ];
+  }
+
+  void remove(Todo todo) {
+    todosNotifier.value =
+        todosNotifier.value.where((t) => t.id != todo.id).toList();
+  }
+
+  void toggleDone(Todo todo) {
+    todosNotifier.value = todosNotifier.value
+        .map((t) => t.id == todo.id ? t.copyWith(completed: !t.completed) : t)
+        .toList();
+  }
+
+  void toggleShowCompletedTodos() {
+    showCompletedTodosNotifier.value = !showCompletedTodosNotifier.value;
+  }
+
+  void resetDate() {
+    _dateService.resetDate();
+  }
+}
