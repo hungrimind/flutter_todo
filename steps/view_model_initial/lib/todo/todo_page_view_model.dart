dbb3e162bc578da493f2b8e498ffce894c4c7cfa
Tadas Petra <tadas@tadaspetra.com>
2025-01-09 09:26:26 -0600

    new todo initial

---
+++ steps/view_model_initial/lib/todo/todo_page_view_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'todo.dart';
+
+class TodoPageViewModel {
+  TodoPageViewModel();
+
+  final ValueNotifier<List<Todo>> todosNotifier = ValueNotifier([]);
+
+  void add(String title) {}
+
+  void remove(Todo todo) {}
+
+  void toggleDone(Todo todo) {}
+}
