ff2daa2d8877974350c768fa4c2b5da6fa68bd1f
Robert Brunhage <robertbrunhage@gmail.com>
2025-01-09 16:46:49 +0100

    chore: more fixes

--- steps/service/lib/main.dart
+++ steps/service/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:demo/todo/todo_page.dart';
-import 'package:demo/utils/locator.dart';
 
 void main() {
-  setupLocator();
-
   runApp(const MyApp());
 }
 
