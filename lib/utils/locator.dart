88ef6c59243d4500d366bd6368ac04db6d2e30f5
Robert Brunhage <robertbrunhage@gmail.com>
2025-02-12 15:12:43 +0100

    chore: simplify folders

--- lib/utils/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:demo/date_service.dart';
-import 'package:get_it/get_it.dart';
-
-final locator = GetIt.instance;
-
-void setupLocator() {
-  locator.registerSingleton(DateService());
-}
